d850f30fa1d1b505154ca6d0612eedb82d089ecd
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-11 15:29:49 +0200

    remove config-service test

--- config-service/src/test/java/com/polarbookshop/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/polarbookshop/configservice/ConfigServiceApplicationTests.java
@@ -21,13 +21,4 @@ class ConfigServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	@Disabled("You can enable this test if your config-repo is publicly available")
-	void defaultConfigurationAvailable() {
-		ResponseEntity<Environment> entity = restTemplate
-				.getForEntity("/application/default", Environment.class);
-		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}
-
 }
